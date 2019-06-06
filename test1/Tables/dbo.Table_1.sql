e6d3d4a5d42697acceda7a3075a2ea447a896970
tozokv <>
2019-06-06 13:38:27 +0200

    ic

--- test1/Tables/dbo.Table_1.sql
+++ test1/Tables/dbo.Table_1.sql
@@ -2,7 +2,15 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
-CREATE TABLE [dbo].[Table_1] ON [PRIMARY]
+CREATE TABLE [dbo].[Table_1] (
+		[ID]          [int] NULL,
+		[ime]         [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[prezime]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[01]          [int] NULL,
+		[02]          [int] NULL,
+		[03]          [int] NULL,
+		[04]          [int] NULL
+) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_1] SET (LOCK_ESCALATION = TABLE)
 GO
